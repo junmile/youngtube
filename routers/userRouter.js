8bbe6dcc2d54ffc06d66e84f3f993a3b8c1dee90
junmile <designeryjun@gmail.com>
2019-12-29 18:13:08 +0900

    commit

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,20 +1,14 @@
 import express from "express";
 import routes from "../routes";
+import { users, userDetail, editProfile, changePassword } from "../controllers/userController";
 
 
 const userRouter = express.Router();
 
-
-const USERS = "/users";
-const USER_DETAIL = "/:id";
-const EDIT_PROFILE = "/edit-profile";
-const CHANGE_PASSWORD = "/change-password";
-
-
-userRouter.get(routes.users, (req,res) => res.send("Users"));
-userRouter.get(routes.userDetail, (req,res) => res.send("User Detail"));
-userRouter.get(routes.editProfile, (req,res) => res.send("Edit Profile"));
-userRouter.get(routes.changePassword, (req,res) => res.send("Change Password"));
+userRouter.get(routes.users, users);
+userRouter.get(routes.userDetail, userDetail);
+userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.changePassword, changePassword);
 
 
 
