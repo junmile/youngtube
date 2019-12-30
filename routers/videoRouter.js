e6faf428e6029cca6f64c485ed45c005374e8c0a
junmile <designeryjun@gmail.com>
2019-12-30 13:18:18 +0900

    video

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -6,7 +6,6 @@ import { videos, upload, videoDetail, editVideo, deleteVideo } from "../controll
 
 const videoRouter = express.Router();
 
-videoRouter.get(routes.videos, videos);
 videoRouter.get(routes.upload, upload);
 videoRouter.get(routes.videoDetail, videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
