f2d7b330faef3302f469fa596fd66016e7ce495e
junmile <designeryjun@gmail.com>
2020-01-22 18:09:52 +0900

    commit

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import passport from "passport";
+import mongoose from "mongoose";
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import { localsMiddleware } from "./middlewares";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
@@ -12,7 +14,10 @@ import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
 
 import "./passport";
+
 const app = express();
+
+const CookieStore = MongoStore(session);
 
 app.use(helmet());
 app.set("view engine", "pug");
@@ -22,7 +27,14 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
-app.use(session({}));
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: true,
+    saveUninitialized: false,
+    store: new CookieStore({ mongooseConnection: mongoose.connection })
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
